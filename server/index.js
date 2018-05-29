7d188ce73391207beb0a831e7952bf9db58d17e6
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-29 16:16:52 -0700

    completed except for Bonus Points

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const movies = require('./top-rated-movies-01.json');
+const lodash = require('lodash');
+
+
+const titles = lodash.map(movies, x => x.title);
+console.log('titles', titles)
---
+++ server/instructor-code/index.js
@@ -0,0 +1,34 @@
+const movies = require('./top-rated-movies-01.json');
+const lodash = require('lodash');
+
+const titles = lodash.map(movies, 'imdbRating');
+// console.log('titles', titles);
+
+
+const user = {
+  name: 'tyler',
+  dob: 'asdf',
+  awesomeness: 'mega',
+  height: ' a perfect 6',
+  children: [{
+    name: 'zoobob',
+    hairColor: 'blonde'
+  }, {
+    name: 'loobob',
+  }]
+};
+
+const u = lodash.pick(user, ['name', 'awesomeness']);
+console.log('-------------- u', u);
+
+const hairColor = lodash.get(user, 'children[2].hairColor');
+console.log('-------------- hairColor', hairColor);
+
+
+const simplifiedMovies = movies.map(x => ({
+  title: x.title,
+  year: x.year
+}));
+
+const moviesByYear = lodash.groupBy(simplifiedMovies, 'year');
+console.log('-------------- moviesByYear', );
